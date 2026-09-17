ea360a97a786a52e036165541f23bf5b4a704a5d
Muhammad Aashir Shehzad <aashirshehzad111@gmail.com>
2026-09-17 16:17:09 +0500

    changed_project_private

--- job_searcher/webapp/src/app/api/admin/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { ADMIN_COOKIE_NAME } from "@/lib/admin-constants";
-
-export async function POST() {
-  const cookieStore = await cookies();
-  cookieStore.delete(ADMIN_COOKIE_NAME);
-  return NextResponse.json({ ok: true });
-}
--- job_searcher/webapp/src/lib/admin-constants.ts
+++
@@ -1,1 +0,0 @@
-export const ADMIN_COOKIE_NAME = "easypezee_admin_session";
